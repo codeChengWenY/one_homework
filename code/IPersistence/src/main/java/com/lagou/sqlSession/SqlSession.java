56d4937ec825e0da9e27b5b4290d36aba87c28e8
程文远 <1042732167@qq.com>
2020-08-22 18:22:37 +0800

    提交作业

---
+++ code/IPersistence/src/main/java/com/lagou/sqlSession/SqlSession.java
@@ -0,0 +1,31 @@
+package com.lagou.sqlSession;
+
+import java.util.List;
+
+public interface SqlSession {
+
+    //查询所有
+    public <E> List<E> selectList(String statementid,Object... params) throws Exception;
+
+    //根据条件查询单个
+    public <T> T selectOne(String statementid,Object... params) throws Exception;
+
+    // 删除
+    public <T> T delete(String statementid,Object... params)  throws Exception;
+
+   // 更新
+    public <T> T update(String statementid,Object... params)  throws Exception;
+
+
+    // 保存
+    public <T> T save(String statementid,Object... params)  throws Exception;
+
+
+
+
+
+    //为Dao接口生成代理实现类
+    public <T> T getMapper(Class<?> mapperClass);
+
+
+}
