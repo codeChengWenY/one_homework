fa050ed772815ec1dc7f6c6680f05cb340c0f5e8
程文远 <1042732167@qq.com>
2020-08-23 16:07:00 +0800

    提交作业

---
+++ code/IPersistence/src/main/java/com/lagou/pojo/MappedStatement.java
@@ -0,0 +1,55 @@
+package com.lagou.pojo;
+
+public class MappedStatement {
+
+    //id标识
+    private String id;
+    //返回值类型
+    private String resultType;
+    //参数值类型
+    private String paramterType;
+    //sql语句
+    private String sql;
+    //获取标签名称
+    private  String   nodeName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getResultType() {
+        return resultType;
+    }
+
+    public void setResultType(String resultType) {
+        this.resultType = resultType;
+    }
+
+    public String getParamterType() {
+        return paramterType;
+    }
+
+    public void setParamterType(String paramterType) {
+        this.paramterType = paramterType;
+    }
+
+    public String getSql() {
+        return sql;
+    }
+
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    public void setNodeName(String nodeName) {
+        this.nodeName = nodeName;
+    }
+}
